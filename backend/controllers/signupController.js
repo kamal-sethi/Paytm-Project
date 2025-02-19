95d56f5f8893e56940f6f1682c98fba9c2adabea
kamal-sethi <kamalsethi018@gmail.com>
2025-02-19 14:27:35 +0530

    created a route for registering the new user and checked validation using ZOD

---
+++ backend/controllers/signupController.js
@@ -0,0 +1,42 @@
+import User from "../model/user.model";
+import zod from "zod";
+import jwt from "jsonwebtoken";
+export const signupController = async (req, res) => {
+  try {
+    const signupSchema = zod.object({
+      username: zod.string(),
+      firstName: zod.string(),
+      lastName: zod.string(),
+      password: zod.string(),
+    });
+
+    const body = req.body;
+
+    const { success } = signupSchema.safeParse(body);
+
+    if (!success) {
+      return res.json({
+        message: "email not valid/incorrect inputs",
+      });
+    }
+
+    const emailExists = await User.findOne({ username: body.username });
+    if (emailExists) {
+      return res.json({ message: "Email already exists/incorrect inputs" });
+    }
+
+    const user = await User.create(body);
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    return res.status(201).json({
+      message: "New user created",
+      user,
+      token: token,
+    });
+  } catch (error) {
+    console.log("error in signup controller");
+    res.status(500).json({
+      message: error.response.data.error,
+    });
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,4 +2,6 @@ import express from "express";
 
 const router = express.Router();
 
+router.post("/signup", signupController);
+
 export default router;
