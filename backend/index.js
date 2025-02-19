cd896042331cd53d5cb9ad1bbd2812f832fc3afe
kamal-sethi <kamalsethi018@gmail.com>
2025-02-19 14:03:15 +0530

    roues made and several packages intaleld

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { connectDB } from "./lib/db.js";
 import dotenv from "dotenv";
+import mainRouter from "./routes/index.route.js";
+import cors from "cors";
 
 const app = express();
 
 dotenv.config();
 
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/", mainRouter);
 app.listen("5000", () => {
   console.log("server is running on 5000");
   connectDB();
