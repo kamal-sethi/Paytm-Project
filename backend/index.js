ba4603886f68b7284e47f35d6153e3019b609ab1
kamal-sethi <kamalsethi018@gmail.com>
2025-02-19 13:05:58 +0530

    connection to database made

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { connectDB } from "./lib/db.js";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
+
 app.listen("5000", () => {
   console.log("server is running on 5000");
+  connectDB();
 });
