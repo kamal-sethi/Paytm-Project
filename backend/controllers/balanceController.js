78eef713dd9a80570ec3fd3179f0e304db4ff427
kamal-sethi <kamalsethi018@gmail.com>
2025-02-25 15:55:22 +0530

    create a route for transfering the balance from one userto another user

--- backend/controllers/balanceController.js
+++ backend/controllers/balanceController.js
@@ -15,3 +15,51 @@ export const getBalance = async (req, res) => {
     });
   }
 };
+
+export const transferMoney = async () => {
+  try {
+    const session = mongoose.startSession();
+
+    session.startTransaction();
+
+    const { amount, To } = req.body;
+    const account = await Account.findOne({ userId: req.userId }).session(
+      session
+    );
+
+    if (!account || !account.balance < amount) {
+      await session.abortTransaction();
+      return res.status(400).json({
+        message: "Insufficient Balance",
+      });
+    }
+
+    const toAccount = await Account.findOne({ userId: To }.session());
+
+    if (!toAccount) {
+      await session.abortTransaction();
+      return res.status(400).json({
+        message: "Invalid Account",
+      });
+    }
+    await Account.updateOne(
+      { userId: req.userId },
+      { $inc: { balance: -amount } }
+    ).session(session);
+
+    await Account.updateOne(
+      { userId: To },
+      { $inc: { balance: amount } }
+    ).session(session);
+
+    await session.commitTransaction();
+    res.json({
+      message: "Transfer successful",
+    });
+  } catch (error) {
+    console.log("error in transfer money controller");
+    res.status(403).json({
+      message: "Error in transfer money controller",
+    });
+  }
+};
--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../middleware/auth";
+import { getBalance } from "../controllers/balanceController";
 
 const router = express.Router();
 
 router.get('/',authMiddleware,getBalance)
+router.post('/transfer',authMiddleware,transferMoney)
 
 export default router;
